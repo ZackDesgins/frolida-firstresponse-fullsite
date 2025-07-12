9e0497fbe474193287380c99622d285665acaafc
ZackDesgins <lawrencemcgee801@gmail.com>
2025-07-12 20:06:59 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "frolida-first-response-rp",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "export": "next export"
+  },
+  "dependencies": {
+    "axios": "^1.6.8",
+    "next": "^14.2.3",
+    "next-auth": "^4.24.6",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0"
+  },
+  "devDependencies": {
+    "autoprefixer": "^10.4.17",
+    "postcss": "^8.4.24",
+    "tailwindcss": "^3.4.1",
+    "typescript": "^5.4.2"
+  }
+}
